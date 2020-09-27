df4f1b8926e3778556ce6053150b0283d60137a6
Adeel <madeelshabbirs@gmail.com>
2020-09-27 15:34:13 +0500

    Webapp

---
+++ public/precache-manifest.e09be76098eb660161d9c3f926bff546.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b07b93035917d2be8973191742f8dab0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "892036ca622e43bb3039",
+    "url": "/static/css/2.7f0a60d8.chunk.css"
+  },
+  {
+    "revision": "141904cfb2fa9e8a6249",
+    "url": "/static/css/main.c8cef0cf.chunk.css"
+  },
+  {
+    "revision": "892036ca622e43bb3039",
+    "url": "/static/js/2.d4fb1279.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/static/js/2.d4fb1279.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "141904cfb2fa9e8a6249",
+    "url": "/static/js/main.34aa4a78.chunk.js"
+  },
+  {
+    "revision": "b3cfd9f5cdf691e24e2f",
+    "url": "/static/js/runtime-main.00ed32f5.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
